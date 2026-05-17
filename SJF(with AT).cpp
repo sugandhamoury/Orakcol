123175c71c97d54e3f5f78a5addc218d7bbc7770
sugandhamoury <sugandharoy65@gmail.com>
2026-05-17 07:27:23 +0600

    Last

--- SJF(with AT).cpp
+++ SJF(with AT).cpp
@@ -1,78 +1,80 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <iomanip>
+
 using namespace std;
 
-struct Process
-{
-    int pid, bt, ct, tat, wt;
+struct Process {
+    int pid, at, bt, ct, tat, wt;
+    bool done;
 };
 
-// Compare Function for Sorting by Burst Time
-bool compare(Process a, Process b)
-{
-    return a.bt < b.bt;
-}
-
-int main()
-{
+int main() {
     int n;
-
-    cout << "Enter number of Processes: ";
+    cout << "Enter number of processes: ";
     cin >> n;
+    Process p;
 
-    Process p[n];
-
-    // Input
-    for(int i = 0; i < n; i++)
-    {
+    // --- Input Section ---
+    for (int i = 0; i < n; i++) {
         p[i].pid = i + 1;
-
-        cout << "\nProcess " << p[i].pid << endl;
-
-        cout << "Burst Time: ";
+        cout << "P" << p[i].pid << " Arrival Time: ";
+        cin >> p[i].at;
+        cout << "P" << p[i].pid << " Burst Time: ";
         cin >> p[i].bt;
+        p[i].done = false;
     }
 
-    // Sorting according to Burst Time
-    sort(p, p + n, compare);
+    // --- SJF Logic Section ---
+    int completed = 0, curr_time = 0;
+    float total_tat = 0, total_wt = 0;
 
-    // Completion Time Calculation
-    p[0].ct = p[0].bt;
+    while (completed != n) {
+        int idx = -1;
+        int min_bt = 1e9; // Initialize with a large value
 
-    for(int i = 1; i < n; i++)
-    {
-        p[i].ct = p[i-1].ct + p[i].bt;
+        // Find process with minimum burst time among arrived processes
+        for (int i = 0; i < n; i++) {
+            if (p[i].at <= curr_time && !p[i].done) {
+                if (p[i].bt < min_bt) {
+                    min_bt = p[i].bt;
+                    idx = i;
+                }
+                // Tie-breaker: process with earlier arrival time
+                if (p[i].bt == min_bt) {
+                    if (p[i].at < p[idx].at) {
+                        idx = i;
+                    }
+                }
+            }
+        }
+
+        if (idx != -1) {
+            p[idx].ct = curr_time + p[idx].bt;
+            p[idx].tat = p[idx].ct - p[idx].at;
+            p[idx].wt = p[idx].tat - p[idx].bt;
+            
+            total_tat += p[idx].tat;
+            total_wt += p[idx].wt;
+            
+            curr_time = p[idx].ct;
+            p[idx].done = true;
+            completed++;
+        } else {
+            // If no process has arrived, increment time
+            curr_time++;
+        }
     }
 
-    // TAT and WT Calculation
-    float total_TAT = 0, total_WT = 0;
-
-    for(int i = 0; i < n; i++)
-    {
-        p[i].tat = p[i].ct;
-
-        p[i].wt = p[i].tat - p[i].bt;
-
-        total_TAT += p[i].tat;
-        total_WT += p[i].wt;
+    // --- Output Section ---
+    cout << "\nPID\tAT\tBT\tCT\tTAT\tWT\n";
+    for (int i = 0; i < n; i++) {
+        cout << p[i].pid << "\t" << p[i].at << "\t" << p[i].bt << "\t" 
+             << p[i].ct << "\t" << p[i].tat << "\t" << p[i].wt << endl;
     }
 
-    // Output
-    cout << "\nP\tBT\tCT\tTAT\tWT\n";
-
-    for(int i = 0; i < n; i++)
-    {
-        cout << "P" << p[i].pid << "\t"
-             << p[i].bt << "\t"
-             << p[i].ct << "\t"
-             << p[i].tat << "\t"
-             << p[i].wt << endl;
-    }
-
-    cout << "\nTotal TAT = " << total_TAT << endl;
-    cout << "Total WT = " << total_WT << endl;
-
-    cout << "Average TAT = " << total_TAT / n << endl;
-    cout << "Average WT = " << total_WT / n << endl;
+    cout << fixed << setprecision(2);
+    cout << "\nAverage Turnaround Time: " << total_tat / n;
+    cout << "\nAverage Waiting Time: " << total_wt / n << endl;
 
     return 0;
 }
